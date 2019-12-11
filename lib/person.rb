4d31ce40469777a464d55674ff1722604661e7a3
Radouane Bahi <radbahi@gmail.com>
2019-12-11 18:52:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,62 @@
 # your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+  def initialize(name)
+    @name = name 
+    @bank_account = 25 
+    @happiness = 8
+    @hygiene = 8
+  @hygiene.clamp(0, 10)
+  @happiness.clamp(0, 10)
+  end
+  
+   def happiness=(happiness_points)
+    @happiness = happiness_points
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+  
+    def hygiene=(hygiene_points)
+    @hygiene = hygiene_points
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
+  
+  def clean?
+    if @hygiene > 7
+      return true 
+    else
+      return false
+    end
+  end
+
+def happy?
+  if @happiness > 7
+    return true 
+else
+  return false 
+end
+end
+
+def get_paid(salary)
+  @bank_account += salary
+  return "all about the benjamins"
+end 
+
+def take_bath
+  self.hygiene += 4
+  return "♪ Rub-a-dub just relaxing in the tub ♫"
+end 
+
+def work_out
+  self.hygiene -= 3
+  self.happiness += 2 
+  return "♪ another one bites the dust ♫"
+end
+
+end
+
+poopy = Person.new("poopy")
